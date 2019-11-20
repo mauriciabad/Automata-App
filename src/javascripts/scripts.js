880fa66879982403eaae120fe3f0310512c0c820
Maurici <hello@mauriciabad.com>
2019-11-20 20:18:11 +0100

    Add Viz.js lib

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,12 +1,17 @@
 /* eslint-disable no-console */
+const Viz = require('viz.js/viz');
+const { Module, render } = require('viz.js/full.render.js');
 const Graph = require('./graph');
 const parse = require('./parser');
 
+const gridElem = document.getElementById('grid');
 const inputElem = document.getElementById('input');
 const outputElem = document.getElementById('output');
 
 let data;
 let graph;
+
+const viz = new Viz({ Module, render });
 
 function readData() {
   data = parse(inputElem.value);
@@ -20,4 +25,10 @@ function readData() {
 
 readData();
 
-inputElem.addEventListener('input', () => { readData(); });
+inputElem.addEventListener('input', readData);
+
+let dotFile = `digraph myAutomaton { rankdir=LR; "" [shape=none] "A" [shape=doublecircle] "B" [shape=doublecircle] "C" [shape=circle] "SINK" [shape=circle] "" -> "C" "A" -> "A" [label="a"] "B" -> "SINK" [label="a [x/ε]"] "C" -> "B" [label="b [y/z]"] "C" -> "A" [label="ε"] "C" -> "A" [label="d"] "C" -> "C" [label="a"] }`;
+
+viz.renderSVGElement(dotFile).then((element) => {
+  gridElem.appendChild(element);
+});
