ccf233418ccaf5ff3d4e178e852c5fc50ab2f560
Maurici <hello@mauriciabad.com>
2019-11-21 01:55:02 +0100

    Read dfa, finite and words in the input

--- src/javascripts/parser.js
+++ src/javascripts/parser.js
@@ -4,13 +4,19 @@
 
 const regxParser = {
   comments: /^ *# *(.*) *$/gm,
-  alphabet: /alphabet: *(.*) */i,
-  stack: /stack: *(.*) */i,
-  states: /states: *((.*)*) */i,
-  final: /final: *((.*)*) */i,
-  transitions: /transitions: *\n([^]*) *end\./i,
-  transition: /^ *([^\n,\->[\]]+) *,? *([^\n,\->[\]]?) *(\[ *([^\n,\->[\]]?) *,? *([^\n,\->[\]]?) *\]?)? *-*>? *([^\n,\->[\]]*)? */gm,
+  alphabet: /alphabet: *(\w*)/i,
+  stack: /stack: *(\w*)/i,
+  states: /states: *((.*)*)/i,
+  final: /final: *((.*)*)/i,
+  transitions: /transitions: *\n([^]+?) *end\./i,
+  transition: /^ *([^\n,\->[\]]+) *,? *(\w?) *(\[ *(\w?) *,? *(\w?) *\]?)? *-*>? *([^\n,\->[\]]*)?/gm,
+  dfa: /dfa: *(\w*)/i,
+  finite: /finite: *(\w*)/i,
+  words: /words: *\n([^]+?) *end\./i,
+  word: /^ *(\w*)[^\w\n]+(\w+)/gm,
 };
+
+const afirmative = ['y', 'ye', 'yes', 'true', '1', 'ok'];
 
 function parse(str) {
   const commentMatches = str.matchAll(regxParser.comments);
@@ -19,6 +25,9 @@ function parse(str) {
   const statesMatch = str.match(regxParser.states);
   const finalMatch = str.match(regxParser.final);
   const transitionsMatches = (str.match(regxParser.transitions) || ['', ''])[1].matchAll(regxParser.transition);
+  const dfaMatch = str.match(regxParser.dfa);
+  const finiteMatch = str.match(regxParser.finite);
+  const wordsMatches = (str.match(regxParser.words) || ['', ''])[1].matchAll(regxParser.word);
 
   return {
     comments: Array.from(commentMatches, (match) => match[1]),
@@ -34,6 +43,12 @@ function parse(str) {
         remove: (match[4] === '_') ? '' : match[4] || '',
         add: (match[5] === '_') ? '' : match[5] || '',
       },
+    })),
+    dfa: (dfaMatch ? afirmative.includes(dfaMatch[1].toLowerCase()) : false),
+    finite: (finiteMatch ? afirmative.includes(finiteMatch[1].toLowerCase()) : false),
+    words: Array.from(wordsMatches, (match) => ({
+      word: match[1] || '',
+      accepted: afirmative.includes(match[2].toLowerCase()),
     })),
   };
 }
