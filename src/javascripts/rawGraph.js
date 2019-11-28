8ac2ef4a0b90b58f017bfe160ad4bd4449e8d7cc
Maurici <hello@mauriciabad.com>
2019-11-28 10:21:22 +0100

    More freedom in parsing

--- src/javascripts/rawGraph.js
+++ src/javascripts/rawGraph.js
@@ -4,11 +4,11 @@ const regxParser = {
   stack: /stacks?:? *(\w*)/i,
   states: /states?:? *((.*)*)/i,
   final: /final:? *((.*)*)/i,
-  transitions: /transitions?:? *\n([^]+?) *end\.?/i,
+  transitions: /transitions?:? *\n([^(end\.)]+?) *end\.?/i,
   transition: /^ *([^\n,\->[\]]+) *,? *(\w?) *(\[ *(\w?) *,? *(\w?) *\]?)? *-*>? *([^\n,\->[\]]*)?/gm,
   dfa: /dfa:? *(\w*)/i,
   finite: /finite:? *(\w*)/i,
-  words: /words?:? *\n([^]+?) *end\.?/i,
+  words: /words?:? *\n([^(end\.)]+?) *end\.?/i,
   word: /^ *(\w*)[^\w\n]+(\w+)/gm,
 };
 
