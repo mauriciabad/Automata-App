5b848e5990b4fe61294f35ae2ec9ebbf57755b5f
Maurici <hello@mauriciabad.com>
2019-12-02 20:17:45 +0100

    Fix typo

--- src/javascripts/node.js
+++ src/javascripts/node.js
@@ -186,7 +186,7 @@ export default class Node {
 
   acceptedStringsRec(accepted, path, visited, visitList) {
     if (visitList.length === 0) return;
-    if (path.length >= 10 || accepted.size() >= 100) return;
+    if (path.length >= 10 || accepted.size >= 200) return;
 
     const adjacency = visitList.pop();
     path.push(adjacency.label);
